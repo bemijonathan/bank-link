d78b27bf6952016f3d3b5bff59ffcedb3145f0b7
bemijonathan <bemijonathan@gmail.com>
2020-09-23 07:40:34 +0100

    MONGO_URL

--- src/models.ts/index.ts
+++ src/models.ts/index.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 export const connect = async () => {
-	await mongoose.connect("mongodb://localhost:27017/myapp?retryWrites=false", {
-		useNewUrlParser: true,
-	});
+	await mongoose.connect(
+		process.env.MONGO_URL ||
+			"mongodb://localhost:27017/myapp?retryWrites=false",
+		{
+			useNewUrlParser: true,
+		}
+	);
 };
 
 //sudo npm install run-rs -g
